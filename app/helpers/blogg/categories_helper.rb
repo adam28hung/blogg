d07660f717ce4d31b2b616de005cec21802ec09c
adam28hung <ivseas@gmail.com>
2015-12-15 23:47:16 +0800

    change style

--- app/helpers/blogg/categories_helper.rb
+++ app/helpers/blogg/categories_helper.rb
@@ -6,5 +6,14 @@ module Blogg
       @categories = Category.where(id: categories.pluck(:id))
       render partial: 'table', locals: { section_title: title } if @categories.count > 0
     end
+
+    def link_to_parent_path(category, options={})
+      if category.parent.present?
+        link_to '上一層', category_path(category.parent), options
+      else
+        link_to '上一層', categories_path, options
+      end
+    end
+
   end
 end
